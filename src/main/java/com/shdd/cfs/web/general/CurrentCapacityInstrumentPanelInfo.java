832e7661279aad3561759afa102495bad1b3ff1b
jiafuzeng <fuzeng.jia@aaadisc.com>
2018-08-28 18:44:13 +0800

    (1)	容量状况6个信息 (2)	获取当前容量状况 部分： 完成单个光盘库节点容量信息获取。

--- src/main/java/com/shdd/cfs/web/general/CurrentCapacityInstrumentPanelInfo.java
+++ src/main/java/com/shdd/cfs/web/general/CurrentCapacityInstrumentPanelInfo.java
@@ -4,6 +4,7 @@ import com.shdd.cfs.dto.dashboard.CurrentDistributedCapacityDetail;
 import com.shdd.cfs.dto.dashboard.CurrentOpticalCapacityDetail;
 import com.shdd.cfs.dto.dashboard.CurrentTapeCapacityDetail;
 import com.shdd.cfs.dto.dashboard.DistributeCapacityStruct;
+import com.shdd.cfs.utils.json.GetJsonMessage;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONArray;
@@ -15,41 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class CurrentCapacityInstrumentPanelInfo {
 
-    @GetMapping(value = "gg/current")
+    @GetMapping(value = "dashboard/curcapacitystatus")
     @ApiOperation(value = "获取仪表盘当前容量信息")
-
 
     public JSONObject SendCurrentCapacity(String SetValue) {
         log.info("SetValue", SetValue);
+        JSONObject getopticalcapacity = new JSONObject();
+        String capacity = "{\"protoname\":\"nodeconnect\"}"; //获取光盘库容量, 节点状态
+        getopticalcapacity = GetJsonMessage.GetJsonStr("192.168.100.199", 8000, capacity);//获取光盘库节点返回报文
         DistributeCapacityStruct currentDisVal = new DistributeCapacityStruct();
         CurrentDistributedCapacityDetail[] disValData = new CurrentDistributedCapacityDetail[1];
         CurrentTapeCapacityDetail currenttapeVal = new CurrentTapeCapacityDetail();
         CurrentOpticalCapacityDetail currentOptVal = new CurrentOpticalCapacityDetail();
-
-        /*给磁带库当前容量赋值*/
+        //给磁带库当前容量赋值
         currenttapeVal.setCapacity(20.34);
         currenttapeVal.setUsedCapacity(15.4);
         currenttapeVal.setDevType("tape");
-        /*给光盘库当前容量赋值*/
-        currentOptVal.setCapacity(100.00);
+        //给光盘库当前容量赋值
+        currentOptVal.setCapacity(Double.parseDouble(getopticalcapacity.getString("totalinfo")));
         currentOptVal.setDevType("optical");
-        currentOptVal.setUsedCapacity(99.99);
-        /*给分布式容量赋值*/
+        currentOptVal.setUsedCapacity(Double.parseDouble(getopticalcapacity.getString(("usedinfo"))));
+        //给分布式容量赋值
         disValData[0] = new CurrentDistributedCapacityDetail();
         disValData[0].setCapacity(20.34);
         disValData[0].setPoolName("xx");
         disValData[0].setUsedCapacity(15.4);
         currentDisVal.setDevType("distributed");
         currentDisVal.setData(disValData);
-
-        /*组织返回JSON数据对象*/
+        //组织返回JSON数据对象
         JSONObject jobject = new JSONObject();
         JSONArray devices = new JSONArray();
         devices.add(currentDisVal);
         devices.add(currenttapeVal);
         devices.add(currentOptVal);
         jobject.accumulate("device", devices);
-        /*给UI 发送 JSON对象 */
+        //给UI 发送 JSON对象
         return jobject;
     }
 }
--- src/main/java/com/shdd/cfs/web/general/TotalCapacityInstrumentPaneInfo.java
+++ src/main/java/com/shdd/cfs/web/general/TotalCapacityInstrumentPaneInfo.java
@@ -2,19 +2,24 @@ package com.shdd.cfs.web.general;
 
 import com.shdd.cfs.dto.dashboard.TotalCapacityInfoDto;
 import com.shdd.cfs.dto.dashboard.TotalStatusInfoDetail;
+import com.shdd.cfs.utils.json.GetJsonMessage;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class TotalCapacityInstrumentPaneInfo {
-      @GetMapping(value = "gg/total")
+      @GetMapping(value = "dashboard/capacitystatus")
       @ApiOperation(value = "发送总容量信息")
 
       public TotalCapacityInfoDto sendTotalCapacityStatusInfo(String TotalInfo) {
             log.info(TotalInfo);
+            JSONObject getnodecapacity = new JSONObject();
+            String nodecapacity = "{\"protoname\":\"nodeconnect\"}"; //获取光盘库容量, 节点状态
+            getnodecapacity = GetJsonMessage.GetJsonStr("192.168.100.199", 8000, nodecapacity);//获取光盘库节点容量信息
 
             TotalCapacityInfoDto capacityStatusInfo = new TotalCapacityInfoDto();
             TotalStatusInfoDetail[] allCapacityInfo = new TotalStatusInfoDetail[1];
@@ -26,7 +31,7 @@ public class TotalCapacityInstrumentPaneInfo {
             allCapacityInfo[0].setAdded(1);
             allCapacityInfo[0].setDistributedUsed(1.00);
             allCapacityInfo[0].setTapeUsed(1.00);
-            allCapacityInfo[0].setDiskUsed(1.00);
+            allCapacityInfo[0].setDiskUsed(Double.parseDouble(getnodecapacity.getString(("usedinfo"))));
             allCapacityInfo[0].setWarning(1);
 
 
