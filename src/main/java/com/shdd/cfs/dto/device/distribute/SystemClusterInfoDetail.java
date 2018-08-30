3d863382ee9a647039f5be7d575ffb14ed179014
贾福增 <fuzeng.jia@aaadisc.com>
2018-08-30 15:15:51 +0800

    更新注释

--- src/main/java/com/shdd/cfs/dto/device/distribute/SystemClusterInfoDetail.java
+++ src/main/java/com/shdd/cfs/dto/device/distribute/SystemClusterInfoDetail.java
@@ -1,10 +1,29 @@
+/*
+ *
+ * THIS FILE IS PART OF COLD FUSION STORAGE NEXUS PROJECT
+ * Copyright (c) 2018 盛和大地公司 版权所有
+ *
+ */
 package com.shdd.cfs.dto.device.distribute;
 
 import lombok.Data;
 
+/**
+ * @author: xphi
+ * @version: 1.0 2018/8/28
+ */
 @Data
 public class SystemClusterInfoDetail {
+    /**
+     * 分布式主机id
+     */
     private Integer id;
+    /**
+     * 分布式节点名称
+     */
     private String name;
+    /**
+     * 分布式节点状态
+     */
     private Integer status;
 }
