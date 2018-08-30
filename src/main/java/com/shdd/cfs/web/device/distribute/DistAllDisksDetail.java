f6066a18acd151e4aa3f96831a75b3313204c0ca
wangpeng <peng.wang@aaadisc.com>
2018-08-30 18:16:30 +0800

    for last commit

--- src/main/java/com/shdd/cfs/web/device/distribute/DistAllDisksDetail.java
+++ src/main/java/com/shdd/cfs/web/device/distribute/DistAllDisksDetail.java
@@ -34,12 +34,16 @@ public class DistAllDisksDetail {
                     name = "count", value = "每页所含最大条目数", required = true)
     })
 
-    public JSONObject DistributeStorageInfo(String value) {
+    public JSONObject DistributeStorageInfo(String value, int page_num, int count) {
         JSONObject jarrary = new JSONObject();
-        NodeInfoDetail[] arrdtail = new NodeInfoDetail[2];
+        NodeInfoDetail[] arrdtail = new NodeInfoDetail[6];
 
         arrdtail[0] = new NodeInfoDetail();
         arrdtail[1] = new NodeInfoDetail();
+        arrdtail[2] = new NodeInfoDetail();
+        arrdtail[3] = new NodeInfoDetail();
+        arrdtail[4] = new NodeInfoDetail();
+        arrdtail[5] = new NodeInfoDetail();
 
         arrdtail[0].setId(1);
         arrdtail[0].setCapacity(50.0);
@@ -47,6 +51,7 @@ public class DistAllDisksDetail {
         arrdtail[0].setName("xx");
         arrdtail[0].setUsed(67.99);
         arrdtail[0].setStatus(1);
+
         arrdtail[1].setId(2);
         arrdtail[1].setCapacity(51.0);
         arrdtail[1].setHostname("Node201");
@@ -54,7 +59,43 @@ public class DistAllDisksDetail {
         arrdtail[1].setUsed(69.99);
         arrdtail[1].setStatus(0);
 
+        arrdtail[2].setId(3);
+        arrdtail[2].setCapacity(51.0);
+        arrdtail[2].setHostname("Node201");
+        arrdtail[2].setName("xx");
+        arrdtail[2].setUsed(69.99);
+        arrdtail[2].setStatus(0);
 
+        arrdtail[3].setId(4);
+        arrdtail[3].setCapacity(51.0);
+        arrdtail[3].setHostname("Node201");
+        arrdtail[3].setName("xx");
+        arrdtail[3].setUsed(69.99);
+        arrdtail[3].setStatus(0);
+
+        arrdtail[4].setId(5);
+        arrdtail[4].setCapacity(51.0);
+        arrdtail[4].setHostname("Node201");
+        arrdtail[4].setName("xx");
+        arrdtail[4].setUsed(69.99);
+        arrdtail[4].setStatus(0);
+
+        arrdtail[5].setId(6);
+        arrdtail[5].setCapacity(51.0);
+        arrdtail[5].setHostname("Node201");
+        arrdtail[5].setName("xx");
+        arrdtail[5].setUsed(69.99);
+        arrdtail[5].setStatus(0);
+
+
+        //计算总页数
+        int totalPage = 0;
+        if (count != 0) {
+            totalPage = 6 / count + 1;
+        }
+        System.out.println(totalPage);
+
+        jarrary.accumulate("totalPage", totalPage);
         jarrary.accumulate("disk", arrdtail);
         return jarrary;
     }
