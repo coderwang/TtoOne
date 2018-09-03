fadc4c528d3f73db5baac2eaf23060062371616c
贾福增 <fuzeng.jia@aaadisc.com>
2018-09-03 13:22:34 +0800

    仪表盘当前容量信息调试通过

--- src/main/java/com/shdd/cfs/web/general/CurrentStorageCapacity.java
+++ src/main/java/com/shdd/cfs/web/general/CurrentStorageCapacity.java
@@ -12,12 +12,17 @@ import com.shdd.cfs.dto.dashboard.CurrentTapeCapacityDetail;
 import com.shdd.cfs.dto.dashboard.DistributeCapacityStruct;
 import com.shdd.cfs.utils.json.DistributeUrlHandle;
 import com.shdd.cfs.utils.json.GetJsonMessage;
+import com.shdd.cfs.utils.xml.iamp.IampRequest;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.dom4j.DocumentException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @Slf4j
@@ -28,33 +33,52 @@ public class CurrentStorageCapacity {
      * @param SetValue
      * @return
      */
+    @Autowired
+    IampRequest iampRequest;
     @GetMapping(value = "api/dashboard/curcapacitystatus")
     @ApiOperation(value = "获取仪表盘当前容量信息", notes = "获取各存储系统当前的容量使用情况，包含总容量和已使用容量")
 
-    public JSONObject SendCurrentCapacity(String SetValue) {
-        log.info("SetValue", SetValue);
-        //光盘库节点容量获取对象
-        JSONObject getopticalcapacity = new JSONObject();
-        //获取光盘库容量, 节点状态
-        String capacity = "{\"protoname\":\"nodeconnect\"}";
-        //获取光盘库节点返回报文
-        getopticalcapacity = GetJsonMessage.GetJsonStr("192.168.100.199", 8000, capacity);
+    public JSONObject SendCurrentCapacity(String SetValue) throws DocumentException {
+//        log.info("SetValue", SetValue);
+//        //光盘库节点容量获取对象
+//        JSONObject getopticalcapacity = new JSONObject();
+//        //获取光盘库容量, 节点状态
+//        String capacity = "{\"protoname\":\"nodeconnect\"}";
+//        //获取光盘库节点返回报文
+//        getopticalcapacity = GetJsonMessage.GetJsonStr("192.168.100.199", 8000, capacity);
+//          Double allOptCapacity = Double.parseDouble(getopticalcapacity.getString("totalinfo"));
+//          Double useOptCapacity = Double.parseDouble(getopticalcapacity.getString("usedinfo"));
+		//获取磁带库容量信息
+        String sessonKey = iampRequest.SessionKey();
+        ArrayList<Integer> alltapelist = iampRequest.all_of_tape_status(sessonKey);
+        Integer alltapesize  = 	alltapelist.size();
+        Double singleTapeCapacity  = 2.5;
+        int fulltape = 0;
+        for(Integer list: alltapelist){
+            if (list == 1){
+                fulltape = fulltape + 1;
+            }  //空白磁带
+        }
+        //磁带库总容量大小
+        Double allTapeCapacity = alltapesize * singleTapeCapacity;
+        //磁带库使用容量大小
+        Double useTapeCapacity = (alltapesize -  fulltape) * singleTapeCapacity;
         //定义Json发送对象
         DistributeCapacityStruct currentDisVal = new DistributeCapacityStruct();
         CurrentTapeCapacityDetail currenttapeVal = new CurrentTapeCapacityDetail();
         CurrentOpticalCapacityDetail currentOptVal = new CurrentOpticalCapacityDetail();
         //给磁带库当前容量赋值
-        currenttapeVal.setCapacity(20.34);
-        currenttapeVal.setUsedCapacity(15.4);
+        currenttapeVal.setCapacity(allTapeCapacity);
+        currenttapeVal.setUsedCapacity(useTapeCapacity);
         currenttapeVal.setDevType("tape");
         //给光盘库当前容量赋值
-        currentOptVal.setCapacity(Double.parseDouble(getopticalcapacity.getString("totalinfo")));
+        currentOptVal.setCapacity(100.00);
         currentOptVal.setDevType("cdstorage");
-        currentOptVal.setUsedCapacity(Double.parseDouble(getopticalcapacity.getString(("usedinfo"))));
+        currentOptVal.setUsedCapacity(50.00);
         //给分布式容量赋值
 
         //获取分布式单个存储池信息
-        JSONObject poolinfo = DistributeUrlHandle.Poolinfo();
+       // JSONObject poolinfo = DistributeUrlHandle.Poolinfo();
         CurrentDistributedCapacityDetail[] disValData = new CurrentDistributedCapacityDetail[1];
         disValData[0] = new CurrentDistributedCapacityDetail();
         disValData[0].setCapacity(23.4);
