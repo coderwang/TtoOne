2cfae5a7b10f3de996810dca6f25502b4414f27d
wangpeng <peng.wang@aaadisc.com>
2018-09-07 14:20:28 +0800

    整理warninginfo代码

--- src/main/java/com/shdd/cfs/web/websocket/WarningDataController.java
+++ src/main/java/com/shdd/cfs/web/websocket/WarningDataController.java
@@ -6,19 +6,19 @@
  */
 package com.shdd.cfs.web.websocket;
 
-import com.shdd.cfs.dto.message.HelloMessage;
 import com.shdd.cfs.dto.message.WarningInfo;
 import com.shdd.cfs.utils.xml.iamp.HttpResult;
 import com.shdd.cfs.utils.xml.iamp.IampRequest;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
 
 /**
  * @author: wangpeng
@@ -34,34 +34,50 @@ public class WarningDataController {
     /**
      * 获取告警信息
      *
-     * @param helloMessage
+     * @param
      * @return
      * @throws Exception
      */
     @Autowired
     IampRequest iampRequest;
+
     @ApiOperation(value = "获取告警信息")
     @MessageMapping("/warning_info")
     @SendTo("/log/warning_info")
-    public JSONObject getWarningDataData(HelloMessage helloMessage) throws Exception {
+    public JSONObject getWarningDataData() throws Exception {
         Thread.sleep(1000); // simulated delay
+        //定义返回对象
+        JSONObject rstObject = new JSONObject();
+        ArrayList warnInfoList = new ArrayList();
+
+        //与下级磁带库系统系统通讯
         String session = iampRequest.SessionKey();
-        HttpResult massagelist = iampRequest.inquiry_task_warn(session);
-        JSONObject warningObj = new JSONObject();
-        JSONArray warningArr = new JSONArray();
-        WarningInfo[] warningInfo = new WarningInfo[3];
-        warningInfo[0] = new WarningInfo();
-        warningInfo[0].setName("distribute");
-        warningInfo[0].setStatus(iampRequest.get_task_warn(massagelist));
-        warningInfo[1] = new WarningInfo();
-        warningInfo[1].setName("tape");
-        warningInfo[1].setStatus(1);
-        warningInfo[2] = new WarningInfo();
-        warningInfo[2].setName("optical");
-        warningInfo[2].setStatus(1);
-        warningArr.add(warningInfo);
-        warningObj.accumulate("status",warningArr);
-        return warningObj;
+        HttpResult logsList = iampRequest.inquiry_task_warn(session);
+        //处理数据
+        WarningInfo tapeWarnInfo = new WarningInfo();
+        tapeWarnInfo.setName("tape");
+        tapeWarnInfo.setStatus(iampRequest.get_task_warn(logsList));
+        //将磁带库告警信息存入数据缓存区
+        warnInfoList.add(tapeWarnInfo);
+
+        //与下级分布式系统通信
+        //处理数据
+        WarningInfo distWarnInfo = new WarningInfo();
+        distWarnInfo.setName("distribute");
+        distWarnInfo.setStatus(0);
+        //将分布式告警信息存入数据缓存区
+        warnInfoList.add(distWarnInfo);
+
+        //与下级光盘库系统通信
+        //处理数据
+        WarningInfo cddiskWarnInfo = new WarningInfo();
+        cddiskWarnInfo.setName("disk");
+        cddiskWarnInfo.setStatus(0);
+        //将分布式告警信息存入数据缓存区
+        warnInfoList.add(cddiskWarnInfo);
+
+        rstObject.accumulate("status", warnInfoList);
+        return rstObject;
     }
 
     //@Scheduled(cron = "0/2 * * * * ? ")//每两秒触发
