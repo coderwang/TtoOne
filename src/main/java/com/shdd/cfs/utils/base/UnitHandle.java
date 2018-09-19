efb27ddb7a0b91fc2132f9fad940d818133bdc8d
jiafuzeng <fuzeng.jia@aaadisc.com>
2018-09-19 12:36:56 +0800

    添加方法： 获取文件夹下所有文件夹名称

--- src/main/java/com/shdd/cfs/utils/base/UnitHandle.java
+++ src/main/java/com/shdd/cfs/utils/base/UnitHandle.java
@@ -1,7 +1,11 @@
 package com.shdd.cfs.utils.base;
 
 
+import com.shdd.cfs.dto.data.DirPathDetailInfo;
+import net.sf.json.JSONObject;
+
 import java.io.File;
+import java.util.ArrayList;
 
 public class UnitHandle {
 
@@ -52,7 +56,6 @@ public class UnitHandle {
 	}
 
 	/**
-	 *
 	 * @param fileDir  根目录
 	 * @param num 递归层次
 	 */
@@ -62,19 +65,15 @@ public class UnitHandle {
 		if (files == null) {// 如果目录为空，直接退出
 			return;
 		}
-		// 遍历，目录下的所有文件
+		// 遍历，目录下的所有文件夹
 		num+=1;
 		for (File f : files) {
 			System.out.println(f.getAbsoluteFile());
 			if (f.isDirectory()) {
-				System.out.println(spliteStr(f.getAbsolutePath()+"+++++++++++++++++++"+ num));
 				consoleFile(f.getAbsolutePath(), num);
-			} else if (f.isFile()) {
-				System.out.println(spliteStr(f.getAbsolutePath())+"+++++++++++++++++++" + num);
 			}
 		}
 	}
-
 	/**
 	 * 截取最后一个/后面的所有字符
 	 * @param Str  要截取的字符串
@@ -84,8 +83,41 @@ public class UnitHandle {
 		return Str.substring(Str.lastIndexOf("\\")+1);
 	}
 
-	public static void main(String[] args) {
-		consoleFile("D:\\cdrskin", 0);
+	/**
+	 * 获取指定文件夹下的所有文件夹名称
+	 * @param pathfolder  指定的文件夹路径
+	 * @return
+	 */
+	public static JSONObject SendPathFolderName(String pathfolder){
+
+		JSONObject rootFolder = new JSONObject();
+		ArrayList<String> folderList = new ArrayList<String>();
+		/* 将目录下的文件夹添加到文件夹列表*/
+		File file = new File(pathfolder);
+		File[] tempList = file.listFiles();
+		for (int i = 0; i < tempList.length; i++) {
+			if (tempList[i].isDirectory()) {
+				String folderAndPath = tempList[i].toString();
+				File fName = new File(folderAndPath.trim());
+				String folderName = fName.getName();
+				folderList.add(folderName);
+			}
+		}
+		/* 将文件夹名赋值给Json数组中*/
+		DirPathDetailInfo[] rootforder = new DirPathDetailInfo[folderList.size()];
+		for(int i = 0; i< folderList.size(); i++){
+			rootforder[i] = new DirPathDetailInfo();
+			rootforder[i].setId(i + 1);
+			rootforder[i].setName(folderList.get(i));
+			System.out.println(folderList.get(i));
+		}
+		/*将文件夹数组塞入Json对象中*/
+		rootFolder.accumulate("folder",rootforder);
+		/* 发送Json 协议*/
+		return  rootFolder;
 	}
 
+	public static void main(String[] args) {
+		//consoleFile("D:\\cdrskin", 0);
+	}
 }
